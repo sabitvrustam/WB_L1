2121a09d7597c003a68322819a7d4b7ad7738607
sabitvrustam <sabitvrustam@gmail.com>
2023-07-17 17:53:47 +0500

    REFACTORING

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,23 +5,28 @@ import (
 )
 
 func main() {
-	//units.SquaresValue()
-	//units.SquaresValueSum()
-	//units.WorkerPul()
-	//units.WriteCh()
-	//units.CloseGoroutine()
-	//units.WrieDataMap()
-	//units.ChangeBit()
-	//units.NumberPipeline()
-	//units.JoiningGroups()
-	//units.ChangeVar()
-	//units.EmptyInterface()
-	//units.Sort()
-	//units.BinarySearch()
-	//units.Counter()
-	//units.Revers()
-	//units.WordConvection()
-	//units.RemoveElementSlice()
-	//units.CallSlip()
-	units.UniqueSymbols()
+	//units.SquaresValue()  //UNIT 2
+	//units.SquaresValueSum()  //UNIT 3
+	//units.WorkerPul() //UNIT 4
+	//units.WriteCh()  //UNIT 5
+	//units.CloseGoroutine()  //UNIT 6
+	//units.WrieDataMap() //UNIT 7
+	//units.ChangeBit()  //UNIT 8
+	//units.NumberPipeline()  //UNIT 9
+	//units.JoiningGroups() //UNIT 10
+	//units.SetIntersection() //UNIT 11
+	//units.CreateSet()       //UNIT 12
+	//units.ChangeVar() //UNIT 13
+	//units.EmptyInterface() // UNIT 14
+	//units.Sort() //UNIT 16
+	//units.BinarySearch() //UNIT 17
+	//units.Counter() //UNIT 18
+	//units.Revers() //UNIT 19
+	//units.WordConvection() //UNIT 20
+	units.Unit21() //UNIT21
+	//units.Calc()   //UNIT 22
+	//units.RemoveElementSlice() //UNIT 23
+	//units.Unit24() // UNIT 24
+	//units.CallSlip() //UNIT 25
+	//units.UniqueSymbols()  UNIT 26
 }
